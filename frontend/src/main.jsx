cac851377e45b38118be4041af466f26b107cbd6
softkleenex <softkleenex1217@gmail.com>
2026-04-14 19:54:37 +0900

    Auto deployment (Production Fixes)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -28,7 +28,7 @@ import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-const GOOGLE_CLIENT_ID = "485345719373-gil7qa14ci4obooh0bcac1pcb3gs4f4q.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "485345719373-keeosnetrubiacu3ii89hh2esq3tvbeo.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
